a2a67603246b644e0a36efdefb11e690b49ff942
ishikawa-pro <ishikawa.pro@gmail.com>
2017-05-21 15:22:03 +0900

    [add]JSONから構造体を初期化できなかった時のErrorを表すためのJSONDecoderError型を定義
    ・JSONDecodeError.swiftにJSONから構造体を初期化できなかった時のErrorを表すためのJSONDecoderError型を定義
    ・.invalidFormatと .missingValueの２つのケースを持つ
    ・.invalidFormat
    　JSONの構造が期待しているものと異なっている状態を表す。
    　連想値としてそのJSONを渡す。
    ・.missingValue
    　構造体の初期化に必要なキーに対する値がJSONに存在しないか、その型が構造体のプロパティの型と異なる状態を表す。
    　連想値としてキーと実際の値をプロパティの連想値として渡す。

---
+++ GitHub_repositories_search/JSONDecodeError.swift
@@ -0,0 +1,16 @@
+//
+//  JSONDecodeError.swift
+//  GitHub_repositories_search
+//
+//  Created by 石川諒 on 2017/05/21.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import Foundation
+
+enum JSONDecodeError : Error{
+    //JSONの構造が期待しているものと異なっている状態
+    case invalidFormat(json: Any)
+    //初期化に必要なキーに対する値がJSONに存在しないか、プロパティの型と異なる場合
+    case missingValue(key: String, actualValue: Any?)
+}
