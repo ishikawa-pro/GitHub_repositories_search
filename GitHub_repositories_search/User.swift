6f58d62ba1ca3863269a1bfc1cb443caacf85cbc
ishikawa-pro <ishikawa.pro@gmail.com>
2017-05-22 07:29:38 +0900

    [add] User型をインスタンス化するイニシャライザを実装
    JSONを渡してイニシャライズする。
    JSONの型は[String: Any]型であることが期待されるので、
    それ以外の型の場合は初期化に失敗する。

--- GitHub_repositories_search/User.swift
+++ GitHub_repositories_search/User.swift
@@ -14,4 +14,34 @@ struct User {
     let id: Int
     let name: String
     let avatarUrl: String
+    
+    init(json: Any) throws{
+        
+        //JSONからAnyへ
+        guard let dictionary = json as? [String : Any] else{
+            throw JSONDecodeError.invalidFormat(json: json)
+        }
+        
+        //Anyからidを取り出す
+        guard let id = dictionary["id"] as? Int else {
+            throw JSONDecodeError.missingValue(
+                key: "id", actualValue: dictionary["id"])
+        }
+        
+        //Anyからnameを取り出す
+        guard let name = dictionary["name"] as? String else {
+            throw JSONDecodeError.missingValue(
+                key: "name", actualValue: dictionary["name"])
+        }
+        
+        //Anyからavatar_urlを取り出す
+        guard let avatarUrl = dictionary["avatar_url"] as? String else {
+            throw JSONDecodeError.missingValue(
+                key: "avatarUrl", actualValue: dictionary["avatar_url"])
+        }
+        
+        self.id = id
+        self.name = name
+        self.avatarUrl = avatarUrl
+    }
 }
