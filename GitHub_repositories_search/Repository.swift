1dd0ebd37a8cdb33380d2524d6143a6c70746c19
ishikawa-pro <ishikawa.pro@gmail.com>
2017-05-21 14:16:33 +0900

    [add] Responseのリポジトリ情報を格納するRepository.swiftを追加
    id, name, full_name, html_url, description, owner(ownerの情報を持つUser型)を格納する

---
+++ GitHub_repositories_search/Repository.swift
@@ -0,0 +1,18 @@
+//
+//  Repository.swift
+//  GitHub_repositories_search
+//
+//  Created by 石川諒 on 2017/05/21.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import Foundation
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let htmlUrl: String
+    let description: String
+    let owner: User
+}
