e2a5c65ae6b8d2adc20fe742e20bd3e67bcaad4c
ishikawa-pro <ishikawa.pro@gmail.com>
2017-05-29 08:55:35 +0900

    [add]検索結果のリポジトリの情報を格納するためのRepositoryViewCellの追加

---
+++ GitHub_repositories_search/RepositoryViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryViewCell.swift
+//  GitHub_repositories_search
+//
+//  Created by 石川諒 on 2017/05/29.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
