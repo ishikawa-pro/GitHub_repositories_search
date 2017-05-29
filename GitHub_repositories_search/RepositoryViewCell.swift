436bb871c623382137b63eff5b153dd5003c2406
ishikawa-pro <ishikawa.pro@gmail.com>
2017-05-29 22:07:10 +0900

    [add]検索結果を表示するカスタムセルを追加
    nameとdescriptionのみlabelで表示

--- GitHub_repositories_search/RepositoryViewCell.swift
+++ GitHub_repositories_search/RepositoryViewCell.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class RepositoryViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var discriptionLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        //titleLabelを複数行表示にする
+        self.titleLabel.numberOfLines = 0
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    //値をセットする関数
+    func setCell(title: String, discription: String) -> Void {
+        self.titleLabel.text = title
+        self.discriptionLabel.text = discription
+        
+        //titleLabelのサイズを調整
+        self.titleLabel.sizeToFit()
     }
 
 }
